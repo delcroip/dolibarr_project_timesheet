fd460c474bcc8ca7f6d54053f2d5938d73d4fc04
delcroip <pmpdelcroix@gmail.com>
2018-07-21 00:51:17 +0200

    FIX: issue when not in custom folder

--- timesheet/core/lib/includeMain.lib.php
+++ timesheet/core/lib/includeMain.lib.php
@@ -35,7 +35,10 @@ if (! $res && file_exists($path."/../../../../main.inc.php")) {
     $res=@include $path.'/../../../../main.inc.php'; //in custom
     $_SERVER["CONTEXT_DOCUMENT_ROOT"]=realpath($path."/../../../../");
 }
-
+if (! $res && file_exists($path."/../../main.inc.php")) {
+    $res=@include $path.'/../../main.inc.php'; //in custom
+    $_SERVER["CONTEXT_DOCUMENT_ROOT"]=realpath($path."/../../");
+}
 
 if (! $res) die("Include of main fails") ;
 
