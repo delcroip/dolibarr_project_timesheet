df3dca83bb9c8ee230b92a42521a8589b85161aa
Patrick Delcroix <patrick@pmpd.eu>
2019-04-08 19:40:49 +0200

    fix: error with invoice (full taxe support activated) and multiple lines

--- htdocs/timesheet/TimesheetProjectInvoice.php
+++ htdocs/timesheet/TimesheetProjectInvoice.php
@@ -173,7 +173,7 @@ $langs->load('timesheet@timesheet');
             if($id > 0  && is_array($resArray)) {
                 $db->commit();
                 $invoicecard=str_replace(
-                                array("require '../../main.inc.php';","<?php"),
+                                array("require '../../main.inc.php';","<?php","\$db->close();"),
                                 "",
                                 file_get_contents(DOL_DOCUMENT_ROOT.'/compta/facture/card.php'));
                 foreach($resArray as $uId =>$userTaskService) {
@@ -237,14 +237,14 @@ $langs->load('timesheet@timesheet');
                         
                         
                         //eval used instead of include because the main.in.php cannot be included twice so it had to be removed from
-                        if($conf->global->TIMESHEET_EVAL_ADDLINE){
+                       // if($conf->global->TIMESHEET_EVAL_ADDLINE){
                             $post_temp = $_POST;
                             $_POST = $postdata;
                             ob_start();
                             eval($invoicecard);
                             ob_end_clean();
                             $_POST = $post_temp;
-                        }
+                       // }
                         
                         if($service['taskTimeList'] <> '' &&  $result>0)$task_time_array[$result] = $service['taskTimeList'];
                     } else $error++;
