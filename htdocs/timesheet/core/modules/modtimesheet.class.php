2aaad0880b8d0a7860524100d70596102aaf8ce9
Patrick Delcroix <pmpdelcroix@gmail.com>
2020-05-22 11:00:31 +0200

    fix: typos, error on deactivation

--- htdocs/timesheet/core/modules/modtimesheet.class.php
+++ htdocs/timesheet/core/modules/modtimesheet.class.php
@@ -391,19 +391,7 @@ class modTimesheet extends DolibarrModules
                         'target' => '',
                         'user' => 2);
                 $r++;
-                $this->menu[$r]=array('fk_menu' => 'fk_mainmenu=timesheet,fk_leftmenu=timesheet',                    // Use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx, fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
-                        'type' => 'left',                                        // This is a Left menu entry
-                        'titre' => 'adminTimeSpent',
-                        'mainmenu' => 'timesheet',
-                        'leftmenu' => 'Timesheet',
-                        'url' => '/timesheet/ProjectTaskTimeList.php',
-                        'langs' => 'timesheet@timesheet',                // Lang file to use(without .lang) by module. File must be in langs/code_CODE/ directory.
-                        'position' => 140,
-                        'enabled' => '$user->admin', // Define condition to show or hide menu entry. Use '$conf->timesheet->enabled' if entry must be visible if module is enabled. Use '$leftmenu == \'system\'' to show if leftmenu system is selected.
-                        'perms' => '1',                                        // Use 'perms' => '$user->rights->timesheet->level1->level2' if you want your menu with a permission rules
-                        'target' => '',
-                        'user' => 2);
-                $r++;
+
                 $this->menu[$r]=array('fk_menu' => 'fk_mainmenu=timesheet,fk_leftmenu=timesheet',                    // Use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx, fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
                         'type' => 'left',                                        // This is a Left menu entry
                         'titre' => 'Timesheetwhitelist',
@@ -593,6 +581,8 @@ class modTimesheet extends DolibarrModules
          */
         public function remove($options = '')
         {
+
+                $sql = array();
                 return $this->_remove($sql, $options);
         }
 }
