efb2a31ecb25d35cd5f24e4ca17143cbe3bda7f0
Patrick Delcroix <pmpdelcroix@gmail.com>
2021-06-06 12:52:56 +0200

    fix: hound

--- htdocs/timesheet/class/TimesheetPublicHoliday.class.php
+++ htdocs/timesheet/class/TimesheetPublicHoliday.class.php
@@ -45,9 +45,9 @@ class TimesheetPublicHolidays extends CommonObject
 
 
     
-   /*** Function to get the user contry code
-     * @param int $userid
-     * @return string countryId
+    /**  Function to get the user contry code
+     * @param int $userId
+     * @return int ok/ko
      * TODO: establishement not yet taken into account because the user are not yet linked to it
      ***/
     public function getUsercountryId($userId){
@@ -144,7 +144,6 @@ class TimesheetPublicHolidays extends CommonObject
                         $curDate = strtotime($yearStop.'-'.$month.'-'.$day);
                     }
                 }
-                
                 $day = date('j',$curDate-$datestart) -1;
                 $this->holidaylist[$day] = array('dayoff' =>true ,'desc' => $desc, 'code' => $code,
                     'prev' => false, 'next' =>  false );
@@ -155,7 +154,6 @@ class TimesheetPublicHolidays extends CommonObject
                 if($day+1 < $timespan){
                     $this->holidaylist[$day+1]['prev'] = true;
                 }
-
             }
             
             $this->db->free($resql);
@@ -167,10 +165,10 @@ class TimesheetPublicHolidays extends CommonObject
  /**
  * function to form a HTMLform line for this timesheet
  *
- *  @param    array               $headers             header to shows
- *  @param      string              $tsUserId            id that will be used for the total
- *  @param     int               $UserId           id of the user timesheet
- *  @return     string                                        HTML result containing the timesheet info
+ *  @param  array   $headers    header to shows
+ *  @param  string  $tsUserId   id that will be used for the total
+ *  @param  int $UserId id of the user timesheet
+ *  @return string  HTML result containing the timesheet info
  */
     public function getHTMLFormLine($headers, $tsUserId, $userId)
     {
@@ -191,20 +189,20 @@ class TimesheetPublicHolidays extends CommonObject
             $value = ($timetype == "hours")?date('H:i', 
                 mktime(0, 0, $holiday['dayoff']*$dayshours*3600)):$holiday['dayoff'];
             $html .= '<th style = "margin: 0;padding: 0;">';
-            if ($conf->global->TIMESHEET_ADD_HOLIDAY_TIME == 1)
+            if ($conf->global->TIMESHEET_ADD_HOLIDAY_TIME == 1){
                 $class = "column_${tsUserId}_${day} user_${userId} line_${tsUserId}_publicholiday";
-                $html .= '<input type = "hidden" class = "'.$class.'"  value = "'.$value.'">';
-                $html .= '<a id = "holiday['.$i.'][0]" ';
-                if ($holiday['dayoff']) {
-                    $html .= 'title="'.$holiday['code'].'-'.$holiday['desc'].'"';
-                    $Color = 'display: block;background-color:#'.$statusColor[APPROVED].'';
-                    $Class = ($holiday['prev'])?'':' noPrevHoliday';
-                    $Class .= ($holiday['next'])?'':' noNextHoliday';
-                    $html .= ' class = "holiday'.$Class.'" style = "'.$Color.'">&nbsp;</a></li>';
-                } else {
-                    $html .= ' class = "holiday" >&nbsp;</a>';
-                }
-           // }
+            }
+            $html .= '<input type = "hidden" class = "'.$class.'"  value = "'.$value.'">';
+            $html .= '<a id = "holiday['.$i.'][0]" ';
+            if ($holiday['dayoff']) {
+                $html .= 'title="'.$holiday['code'].'-'.$holiday['desc'].'"';
+                $Color = 'display: block;background-color:#'.$statusColor[APPROVED].'';
+                $Class = ($holiday['prev'])?'':' noPrevHoliday';
+                $Class .= ($holiday['next'])?'':' noNextHoliday';
+                $html .= ' class = "holiday'.$Class.'" style = "'.$Color.'">&nbsp;</a></li>';
+            } else {
+                $html .= ' class = "holiday" >&nbsp;</a>';
+            }
             $html .= "</th>\n";
             $i++;
         }
